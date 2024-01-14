65084b2593ef461c770f4ddf598f0d52d5b4f8c7
Dava Ardiansyah <davaardiansyah14042007@gmail.com>
2024-01-14 14:22:54 +0700

    membuat autentifikasi

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\Alumni\PageController::class, 'index'])
+    ->name('homepage');
+Route::middleware(['auth'])->group(function () {
+    Route::middleware('operator')->name('operator.')->prefix('operator')->group(function () {
+        Route::get('/dashboard', [App\Http\Controllers\Operator\PageController::class, 'index'])
+            ->name('dashboard');
+    });
+
+
+    Route::middleware('admin')->name('admin.')->prefix('admin')->group(function () {
+        Route::get('/dashboard', [App\Http\Controllers\Admin\PageController::class, 'index'])
+            ->name('dashboard');
+    });
 });
