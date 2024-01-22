3c866599ec766acacf1b1e6a57ccd24c883c605e
Acid <azrasyad65@gmail.com>
2024-01-22 07:15:02 +0700

    tes

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,7 @@ Route::middleware(['auth'])->group(function () {
             ->name('Admin.dashboard');
     });
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
