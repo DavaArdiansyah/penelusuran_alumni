02908a81616e6ffe0413de403adea63041b09665
revan d <juliuussyb69@gmail.com>
2024-01-22 15:08:51 +0700

    fix

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,6 +52,7 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'no_telp' => ['required', 'regex:/^(08|\+628)[0-9]{8,10}$/'],
         ]);
     }
 
@@ -67,6 +68,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'no_telp' => $data['no_telp'],
         ]);
     }
 }
