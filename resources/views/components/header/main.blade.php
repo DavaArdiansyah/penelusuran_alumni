c0197586bb5fd4ac71e502f34bd70a7f58aadb5e
DavaArdiansyah <davaardiasnyah14042007@gmail.com>
2024-01-23 11:13:11 +0700

    resolve problem

---
+++ resources/views/components/header/main.blade.php
@@ -0,0 +1,33 @@
+<div class="header">
+    <div class="header-left">
+        <div class="menu-icon dw dw-menu"></div>
+    </div>
+    <div class="header-right">
+        @if (!auth()->user())
+            <!--button login-->
+            <div class="dashboard-setting user-notification">
+                <div class="dropdown">
+                    <a class="dropdown-toggle no-arrow" href="javascript:;" data-toggle="right-sidebar">
+                        <i class="dw dw-settings2"></i>
+                    </a>
+                </div>
+            </div>
+            <!--button register-->
+            <div class="user-notification">
+                <div class="dropdown">
+                    <a class="dropdown-toggle no-arrow" href="#" role="button" data-toggle="dropdown">
+                        <i class="icon-copy dw dw-notification"></i>
+                        <span class="badge notification-active"></span>
+                    </a>
+                </div>
+            </div>
+        @endif
+        @if (auth()->user())
+            @include('components.header.infoNonUser')
+        @endif
+        <div class="github-link">
+            <a href="https://github.com/dropways/deskapp" target="_blank"><img src="vendors/images/github.svg"
+                    alt=""></a>
+        </div>
+    </div>
+</div>
